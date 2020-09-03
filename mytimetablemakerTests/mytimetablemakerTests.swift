b5e5087e2b98f0c314a7c61b58fbe8b51ef35263
fcb1899 <fcb1899v3@gmail.com>
2020-09-03 23:20:36 +0900

    Initial Commit

---
+++ mytimetablemakerTests/mytimetablemakerTests.swift
@@ -0,0 +1,34 @@
+//
+//  mytimetablemakerTests.swift
+//  mytimetablemakerTests
+//
+//  Created by 中島正雄 on 2020/09/03.
+//  Copyright © 2020 com.nakajimamasao. All rights reserved.
+//
+
+import XCTest
+@testable import mytimetablemaker
+
+class mytimetablemakerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
