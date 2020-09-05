517945b0611a09d991234296c5195fd3a89cccc8
fcb1899 <fcb1899v3@gmail.com>
2020-09-05 15:49:06 +0900

    9/5

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  mytimetablemaker
+//
+//  Created by 中島正雄 on 2020/09/05.
+//  Copyright © 2020 com.nakajimamasao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ mytimetablemaker/TimetableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  TimetableViewController.swift
+//  mytimetablemaker
+//
+//  Created by 中島正雄 on 2020/09/05.
+//  Copyright © 2020 com.nakajimamasao. All rights reserved.
+//
+
+import UIKit
+
+class TimetableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+import UIKit
+
+extension UIView {
+
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
